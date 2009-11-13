750779f839df5772190420043936fa81ac067674
omargallob <omargallob@gmail.com>
2009-11-13 12:19:35 +0000

    fucked up stylesheets fixed

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,7 +2,11 @@ class Admin::BaseController < ApplicationController
  before_filter :login_required
  require_role ["admin",'eandv']
  layout 'admin' 
- 
+ include AuthenticatedSystem
+  # You can move this into a different controller, if you wish.  This module gives you the require_role helpers, and others.
+ include RoleRequirementSystem
+  
+  
  def setmeta
    set_meta_tags :site => 'Admin - E&V NPB - '+ controller_name.capitalize + ' - ' + action_name
  end
