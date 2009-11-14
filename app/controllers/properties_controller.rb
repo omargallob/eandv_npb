82f034571d4775d4bc06397c6667033bbb1f068f
omargallob <omargallob@gmail.com>
2009-11-14 20:07:31 +0000

    pre nearest schools

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,7 +1,15 @@
 class PropertiesController < ApplicationController
   def index
+
      @properties = Property.find(:all, :include => 'location',:order => 'locations.region')
      @page = Page.find_by_name('properties')
+      if @page.metatag
+         set_meta_tags :title =>  @page.title,
+                     :description => @page.metatag.description,
+                     :keywords =>  @page.metatag.keywords 
+       else
+         set_meta_tags :title =>  @page.title
+      end
      @subpages = @page.subpages
       respond_to do |format|
           format.html # show.html.erb
@@ -10,8 +18,12 @@ class PropertiesController < ApplicationController
   end
 
   def show
+
     @property = Property.find_by_id(params[:id])
     @page = Page.find_by_name('properties')
+
+       set_meta_tags :title =>  "("+@property.location.zipcod+") "+@property.location.region+" - "+ @property.title
+
   end 
   
   def feed
